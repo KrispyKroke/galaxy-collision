d2b5bca73373c62db9b4145956d4fee0969d58b3
Jared Kroke <jared.kroke@gmail.com>
2023-05-19 12:15:17 -0500

    Created Description component and added styling to element. Adjusted appearance of background image as well.

--- src/Components/App/App.jsx
+++ src/Components/App/App.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import './App.css';
 
+import Description from '../Description/Description.jsx';
+
 function App() {
+  
   return (
     <div className="App">
-      
+      <Description />
     </div>
   );
 }
---
+++ src/Components/Description/Description.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Description.css';
+
+function Description() {
+
+    return (
+        <div id='text'>
+            <h3>THIS SITE HAS THE MOST ACCURATE
+                MILE TRACKER FOR WHEN THE MILKY WAY 
+                AND ANDROMEDA GALAXY WILL COLLIDE!
+            </h3>
+        </div>
+    )
+}
+
+export default Description;
