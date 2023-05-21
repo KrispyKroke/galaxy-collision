1fdc926a1dad9b81f5141b796b04458ef0ca1f0b
Jared Kroke <jared.kroke@gmail.com>
2023-05-21 14:44:38 -0500

    Implemented mile ticker and added new space favicon. Project completed. Going to push changes and update the README.

--- src/Components/MilesDisplay/MilesDisplay.jsx
+++ src/Components/MilesDisplay/MilesDisplay.jsx
@@ -8,8 +8,12 @@ function MilesDisplay() {
 
     const [milesRemaining, setMilesRemaining] = useState('');
 
+    // When the page is visited, the current number of miles remaining is 
+    // calculated.  After that, the new distance is recalculated at an 
+    // interval of 1 second for as long as the user is on the page.
     useEffect(() => {
         calculateMiles();
+        setInterval(calculateMiles, 1000);
     }, []);
 
     /* When this function is called, it uses a fixed reference date 
@@ -19,14 +23,14 @@ function MilesDisplay() {
     collision of the galaxies. */
     function calculateMiles() {
         const collisionDateInSeconds = 211856265808983360;
-        const todayInSeconds = Date.now() / 1000;
+        const todayInSeconds = Math.round(Date.now() / 1000);
         const timeLeft = collisionDateInSeconds - todayInSeconds;
-        setMilesRemaining(timeLeft * 69);
+        setMilesRemaining(Math.round(timeLeft * 69));
     }
 
     return (
         <div className="MilesContainer">
-            <h2>{milesRemaining} MILES UNTIL COLLISION!</h2>
+            <h2>{milesRemaining.toLocaleString()} MILES UNTIL COLLISION!</h2>
         </div>
     );
 }
